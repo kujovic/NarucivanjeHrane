7bda3a6b4c3d54f5ff75ab705575321e0fa51a9a
kujovic <Stefan.Kujovic@saga.rs>
2021-12-07 13:44:23 +0000

    Docker-compose

---
+++ narucivanje-hrane/narucivanjehrane.sql
@@ -0,0 +1,4 @@
+USE narucivanjehrane;
+CREATE TABLE osoba (id int,ime varchar(255),narucivanje varchar(255));
+INSERT into osoba (id,ime,narucivanje) values (1,'Sara','salata');
+INSERT into osoba (id,ime,narucivanje) values (2,'Stefan','rostilj');
